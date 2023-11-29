f671766a88b281e231bc5eef536db59ce5130f41
Steveen <stivenjimenez37@gmail.com>
2023-11-28 22:44:00 -0500

    Se realiza el maquetado y funcionamiento de la calculadora utilizando el useState y algunas funciones handle que serviran para realizar los calculos o retornar a punto 0 el estado de la calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App =()=> {
+  const [result, setResult] = useState('');
+  
+  const handleClick = (number) => {
+    setResult(result + number.target.value)
+  }
+  const handleDelete = () => {
+    setResult(result.slice(0,-1))
+  }
+  const handleClear = () => {
+    setResult('')
+  }
+  const handleEqual = () => {
+    setResult(eval(result))
+  }
+  const handlePercentage = () => {
+    
+    setResult(eval(result/100))
+  }
+ 
 
   return (
     <>
-      
+      <div className="Container">
+        <input className="display" value={result}></input>
+        <div className="containerKeys">
+          <div className="keys">
+            <input type='button' value="7" className="button" onClick={handleClick}></input>
+            <input type='button' value="8" className="button" onClick={handleClick}></input>
+            <input type='button' value="9" className="button" onClick={handleClick}></input>
+            <input type='button' value="Del" className="button" onClick={handleDelete}></input>
+            <input type='button' value="AC" className="button" onClick={handleClear}></input>
+          </div>
+          <div className="keys">
+            <input type='button' value="4" className="button" onClick={handleClick}></input>
+            <input type='button' value="5" className="button" onClick={handleClick}></input>
+            <input type='button' value="6" className="button" onClick={handleClick}></input>
+            <input type='button' value="*" className="button" onClick={handleClick}></input>
+            <input type='button' value="+" className="button" onClick={handleClick}></input>
+          </div>
+          <div className="keys">
+            <input type='button' value="1" className="button" onClick={handleClick}></input>
+            <input type='button' value="2" className="button" onClick={handleClick}></input>
+            <input type='button' value="3" className="button" onClick={handleClick}></input>
+            <input type='button' value="-" className="button" onClick={handleClick}></input>
+            <input type='button' value="/" className="button" onClick={handleClick}></input>
+          </div>
+          <div className="keys">
+            <input type='button' value="0" className="button" onClick={handleClick}></input>
+            <input type='button' value="." className="button" onClick={handleClick}></input>
+            <input type='button' value="%" className="button" onClick={handleClick} ></input>
+            <input type='button' value="=" className="button" onClick={handleEqual}></input>
+            <input type='button' value="%" className="button" onClick={handlePercentage} id='btn'></input> 
+          </div>
+        </div>
+      </div>
+      <h2>El boton rojo corresponde al porcentaje</h2>
     </>
-  )
+  );
 }
 
-export default App
